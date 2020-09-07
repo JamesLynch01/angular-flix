0775fe1e878e2b53cdba99a3a0237cf85797c53f
JamesLynch01 <Jamesblynch94@gmail.com>
2020-09-07 16:29:23 -0500

    done with services

--- src/app/services/movie-api.service.ts
+++ src/app/services/movie-api.service.ts
@@ -6,14 +6,15 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 })
 export class MovieApiService {
 
-  private readonly URL = 'https://api.themoviedb.org/3/';
+  private readonly URL = 'https://api.themoviedb.org/3/search/multi';
   private readonly API_KEY = '5a470fd8928415f9a6a9d110a9d0e490';
   constructor(private httpClient: HttpClient) { }
 
-  get(path: string): Promise<any[]>{
+  get(searchTerm: string): Promise<any>{
     const params: HttpParams = new HttpParams()
-    .append('api_key', this.API_KEY);
-    
-    return this.httpClient.get<any[]>(`${this.URL}${path}`, {params}).toPromise();
+    .append('api_key', this.API_KEY)
+    .append('query', searchTerm);
+
+    return this.httpClient.get<any[]>(`${this.URL}`, {params}).toPromise();
   }
 }
